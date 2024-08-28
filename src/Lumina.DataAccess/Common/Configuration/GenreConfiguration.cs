46b646c9dad36cd5d66873644c66199c89983f0e
Binary Alley <wc0vp828w5@proton.me>
2024-08-28 21:49:51 +0300

    Specifies Genres and Tags configurations

---
+++ src/Lumina.DataAccess/Common/Configuration/GenreConfiguration.cs
@@ -0,0 +1,26 @@
+﻿#region ========================================================================= USING =====================================================================================
+using Lumina.Application.Common.Models.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+#endregion
+
+namespace Lumina.DataAccess.Common.Configuration;
+
+/// <summary>
+/// Configures the entity mapping for the <see cref="GenreDto"/> entity.
+/// </summary>
+public class GenreConfiguration : IEntityTypeConfiguration<GenreDto>
+{
+    #region ===================================================================== METHODS ===================================================================================
+    /// <summary>
+    /// Configures the <see cref="GenreDto"/> entity.
+    /// </summary>
+    /// <param name="builder">The builder to be used to configure the entity.</param>
+    public void Configure(EntityTypeBuilder<GenreDto> builder)
+    {
+        builder.ToTable("Genres");
+        builder.HasKey(genre => genre.Name);
+        builder.Property(genre => genre.Name).IsRequired().HasMaxLength(50);
+    }
+    #endregion
+}
---
+++ src/Lumina.DataAccess/Common/Configuration/TagConfiguration.cs
@@ -0,0 +1,26 @@
+﻿#region ========================================================================= USING =====================================================================================
+using Lumina.Application.Common.Models.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+#endregion
+
+namespace Lumina.DataAccess.Common.Configuration;
+
+/// <summary>
+/// Configures the entity mapping for the <see cref="TagDto"/> entity.
+/// </summary>
+public class TagConfiguration : IEntityTypeConfiguration<TagDto>
+{
+    #region ===================================================================== METHODS ===================================================================================
+    /// <summary>
+    /// Configures the <see cref="TagDto"/> entity.
+    /// </summary>
+    /// <param name="builder">The builder to be used to configure the entity.</param>
+    public void Configure(EntityTypeBuilder<TagDto> builder)
+    {
+        builder.ToTable("Tags");
+        builder.HasKey(tag => tag.Name);
+        builder.Property(tag => tag.Name).IsRequired().HasMaxLength(50);
+    }
+    #endregion
+}
