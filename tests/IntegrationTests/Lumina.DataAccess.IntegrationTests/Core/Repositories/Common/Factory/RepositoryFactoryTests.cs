a3011297ca0ceb0978bd20f26140c04300465771
Binary Alley <wc0vp828w5@proton.me>
2024-08-28 21:51:57 +0300

    Adds Data Access Layer integration tests

---
+++ tests/IntegrationTests/Lumina.DataAccess.IntegrationTests/Core/Repositories/Common/Factory/RepositoryFactoryTests.cs
@@ -0,0 +1,25 @@
+﻿#region ========================================================================= USING =====================================================================================
+using Lumina.DataAccess.Core.Repositories.Common.Factory;
+using System.Diagnostics.CodeAnalysis;
+#endregion
+
+namespace Lumina.DataAccess.IntegrationTests.Core.Repositories.Common.Factory;
+
+/// <summary>
+/// Contains integration tests for the <see cref="RepositoryFactory"/> class.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public class RepositoryFactoryTests
+{
+    #region ===================================================================== METHODS ===================================================================================
+    [Fact]
+    public void RepositoryFactoryImplementsRepositoryFactoryInterface_InterfaceIsImplemented_ReturnsTrue()
+    {
+        // Arrange
+        var repositoryFactory = new RepositoryFactory(null!);
+        
+        // Assert
+        Assert.True(repositoryFactory is IRepositoryFactory);
+    }
+    #endregion
+}
---
+++ tests/IntegrationTests/Lumina.DataAccess.IntegrationTests/Core/UoW/UnitOfWorkTests.cs
@@ -0,0 +1,32 @@
+﻿
+#region ========================================================================= USING =====================================================================================
+using Lumina.Application.Common.DataAccess.UoW;
+using Lumina.DataAccess.Common.DependencyInjection;
+using Lumina.DataAccess.Core.UoW;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
+#endregion
+
+namespace Lumina.DataAccess.IntegrationTests.Core.UoW;
+
+/// <summary>
+/// Contains integration tests for the <see cref="UnitOfWork"/> class.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public class UnitOfWorkTests
+{
+    #region ===================================================================== METHODS ===================================================================================
+    [Fact]
+    public void UnitOfWorkImplementsUnitOfWorkInterface_InterfaceIsImplemented_ReturnsTrue()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        DataAccessLayerServices.AddDataAccessLayerServices(services);
+        var serviceProvider = services.BuildServiceProvider();
+        var unitOfWork = (UnitOfWork)serviceProvider.GetRequiredService<IUnitOfWork>();
+
+        // Assert
+        Assert.True(unitOfWork is IUnitOfWork);
+    }
+    #endregion
+}
