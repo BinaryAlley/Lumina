651969ea4115ed9da6cff852812640b555d4b066
Binary Alley <wc0vp828w5@proton.me>
2025-03-01 21:27:54 +0200

    Removes library scan file status database column

--- src/Lumina.DataAccess/Common/Migrations/20250301192527_RemovesLibraryScanFileStatusColumn.Designer.cs
+++ src/Lumina.DataAccess/Common/Migrations/20250301192527_RemovesLibraryScanFileStatusColumn.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Lumina.DataAccess.Core.UoW;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Lumina.DataAccess.Common.Migrations
 {
     [DbContext(typeof(LuminaDbContext))]
-    partial class LuminaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250301192527_RemovesLibraryScanFileStatusColumn")]
+    partial class RemovesLibraryScanFileStatusColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
