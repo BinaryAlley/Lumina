9b099cccc6ff3cf1d3fbe265b6399f3918e4d23e
Binary Alley <wc0vp828w5@proton.me>
2026-09-06 10:17:01 -0700

    Fixes the book editing and written content errors tests

--- tests/IntegrationTests/Lumina.Presentation.Web.IntegrationTests/Core/Endpoints/Library/WrittenContentLibrary/BookLibrary/Books/EditBook/EditBookViewEndpointTests.cs
+++ tests/IntegrationTests/Lumina.Presentation.Web.IntegrationTests/Core/Endpoints/Library/WrittenContentLibrary/BookLibrary/Books/EditBook/EditBookViewEndpointTests.cs
@@ -1,5 +1,7 @@
 #region ========================================================================= USING =====================================================================================
+using Lumina.Presentation.Web.Common.DTO.WrittenContentLibrary.BookLibrary;
 using Lumina.Presentation.Web.Core.Endpoints.Library.WrittenContentLibrary.BookLibrary.Books.EditBook;
+using Lumina.Presentation.Web.Fixtures.Common.DTO.WrittenContentLibrary.BookLibrary;
 using Lumina.Presentation.Web.Fixtures.Common.TestHelpers;
 using Lumina.Presentation.Web.IntegrationTests.Common.Setup;
 using System;
@@ -18,6 +20,7 @@ namespace Lumina.Presentation.Web.IntegrationTests.Core.Endpoints.Library.Writte
 public class EditBookViewEndpointTests : IClassFixture<LuminaWebFactory>
 {
     private readonly LuminaWebFactory _apiFactory;
+    private readonly BookDetailsDtoFixture _bookDetailsDtoFixture = new();
 
     /// <summary>
     /// Initializes a new instance of the <see cref="EditBookViewEndpointTests"/> class.
@@ -33,10 +36,13 @@ public class EditBookViewEndpointTests : IClassFixture<LuminaWebFactory>
     {
         // Arrange
         _apiFactory.ApiClientStub.Reset();
+        BookDetailsDto expectedBook = _bookDetailsDtoFixture.Create();
+        expectedBook.Metadata!.Title = "A book";
+        _apiFactory.ApiClientStub.RegisterGetResponse($"books/{expectedBook.Id}", expectedBook);
         AuthenticatedWebClient webClient = await WebTestHelpers.CreateAuthenticatedClientAsync(_apiFactory);
 
         // Act
-        HttpResponseMessage response = await webClient.Client.GetAsync($"/en-us/library/written-content-library/books-library/books/{Guid.NewGuid()}");
+        HttpResponseMessage response = await webClient.Client.GetAsync($"/en-us/library/written-content-library/books-library/books/{expectedBook.Id}");
         string content = await response.Content.ReadAsStringAsync();
 
         // Assert
